eefddde1762a9ebbef7094be80863b17083d08c9
z0r0v <3767082@mail.ru>
2020-12-09 03:00:32 +0300

    uploaded app

--- index.js
+++ index.js
@@ -22,22 +22,62 @@ const config = {
 class App {
     constructor({ n, numbOfCell, playerAccount, app, h3, cellsBox, cellsBoxItem, }) {
         this.n = n;
+
+        this.numbOfCell = numbOfCell;
+        this.playerAccount = playerAccount;
+
+
         this.app = app;
         this.h3 = h3;
+
         this.cellsBox = cellsBox;
-        this.numbOfCell = numbOfCell;
+
         this.cellsBoxItem = cellsBoxItem;
-        this.playerAccount = playerAccount;
 
         this.data = [];
         this.newData = [];
 
+
+        this.appInit();
+    };
+
+    handlerCountUpClick = (evt) => {
+        this.playerAccount = this.playerAccount + parseInt(evt.target.innerText);
+        this.renderCount();
+    };
+
+    appInit() {
+        this.renderCount();
         this.dataCreate();
         this.randomChangeCell();
+    }
 
-        this.render();
-
+    getRandomNumbers(max, min) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     };
+
+    getRandomItemArray(items, value, data) {
+
+        if (items.length != 0) {
+            const item = items[Math.floor(Math.random() * items.length)];
+
+            items.splice(items.indexOf(item), 1);
+            data[item?.id].value = value;
+        }
+    };
+
+    randomChangeCell() {
+        this.newData = [...this.data];
+        setInterval(() => {
+            const randomNumber = this.getRandomNumbers(3, 0);
+
+            this.getRandomItemArray(this.newData, randomNumber, this.data);
+
+            this.cellsBox.innerHTML = "";
+            this.render(this.data);
+
+        }, 2000)
+    }
 
     dataCreate() {
         for (let i = 0; i <= this.numbOfCell - 1; i++) {
@@ -48,9 +88,8 @@ class App {
         }
     };
 
-    render() {
-        const items = this.data;
-
+    render(data) {
+        const items = data;
         items.map(it => {
             const { value } = it;
             const item = document.createElement('div');
@@ -61,34 +100,18 @@ class App {
 
             item.appendChild(itemP);
             this.cellsBox.appendChild(item);
+
+            item.addEventListener('click', this.handlerCountUpClick);
+
+
         });
-
     };
 
-    getRandom(max, min) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    };
-
-    randomChangeCell() {
-        setInterval(() => {
-            const randomNumber = this.getRandom(3, 0);
-            const randomId = this.getRandom(this.numbOfCell, 0);
-
-            console.log(randomId);
-
-
-            this.data.find(it => {
-
-                if (it.id === randomId && it.value === "") {
-                    it.value = randomNumber;
-                }
-
-            });
-
-            this.cellsBox.innerHTML = "";
-            this.render();
-        }, 2000)
-
+    renderCount() {
+        const span = document.createElement('span')
+        this.h3.innerText = 'Player Account: ';
+        this.h3.appendChild(span);
+        span.innerText = this.playerAccount;
     }
 
 }
